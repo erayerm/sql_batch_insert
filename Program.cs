182faada78657b8f2d727d0d939d6e7595ef0cc0
erayerm <ermemiseray@gmail.com>
2023-07-11 13:55:25 +0300

    final

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
 using System.Data;
-using Newtonsoft.Json;
 using System.IO;
-using System.Linq;
-using System.Collections.Generic;
-//Newtonsoft.Json (13.0.3) ve MySql.Data (8.0.33) NuGet'lerini kullandım.
-
+using Newtonsoft.Json.Linq;
 
 namespace sql_batch_insert
 {
@@ -15,101 +11,53 @@ namespace sql_batch_insert
         static void Main(string[] args)
         {
             string ConnectionString = "Server=127.0.0.1;Database=deneme;User Id=root;Password=12345";
-            //string Command = "INSERT INTO Persons (FirstName, LastName, Address, City ) VALUES (@FirstName, @LastName, @Address, @City);";
             string firstName, lastName, address, city;
-            int counter = 0;
 
-            //mysql server ile baglanti yapiliyor
             using (MySqlConnection mConnection = new MySqlConnection(ConnectionString))
             {
-                try
+                mConnection.Open();
+
+                string jsonContent = File.ReadAllText("../../../file.json");
+                //alınan .json dosyası parse'lanıp jsonArray'e aktarılıyor
+                JArray jsonArray = JArray.Parse(jsonContent);
+                DataTable temporaryTable = new DataTable();
+
+                temporaryTable.Columns.Add("FirstName");
+                temporaryTable.Columns.Add("LastName");
+                temporaryTable.Columns.Add("Address");
+                temporaryTable.Columns.Add("City");
+                //array'daki her sey gecici tabloya aktariliyor
+                foreach (JObject jsonObject in jsonArray)
                 {
-                    mConnection.Open();
-                    //json dosyası acilip okunup string olarak json degiskenine aktariliyor
-                    StreamReader r = new StreamReader("../../../file.json");
-                    string json = r.ReadToEnd();
-                    //newtonsoft.json kutuphanesiyle json formundaki string slice'lanıp array'e atiliyor
-                    dynamic array = JsonConvert.DeserializeObject(json);
+                    lastName = jsonObject["lastname"].ToString();
+                    firstName = jsonObject["firstname"].ToString();
+                    address = jsonObject["address"].ToString();
+                    city = jsonObject["city"].ToString();
 
-                    DataTable temporaryTable = new DataTable();
-                    temporaryTable.Columns.Add("FirstName");
-                    temporaryTable.Columns.Add("LastName");
-                    temporaryTable.Columns.Add("Address");
-                    temporaryTable.Columns.Add("City");
-                    
-                    //array'daki her sey tabloya aktariliyor
-                    foreach (var item in array)
+                    temporaryTable.Rows.Add(firstName, lastName, address, city);
+                }
+
+                using (MySqlTransaction tran = mConnection.BeginTransaction(System.Data.IsolationLevel.Serializable))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
                     {
-                        firstName = item.firstname;
-                        lastName = item.lastname;
-                        address = item.address;
-                        city = item.city;
-
-                        temporaryTable.Rows.Add(firstName, lastName, address, city);
-
-                        //using (MySqlCommand myCmd = new MySqlCommand(Command, mConnection))
-                        //{
-                        //    myCmd.CommandType = CommandType.Text;
-                        //    myCmd.Parameters.AddWithValue("@FirstName", item.firstname);
-                        //    myCmd.Parameters.AddWithValue("@LastName", item.lastname);
-                        //    myCmd.Parameters.AddWithValue("@Address", item.address);
-                        //    myCmd.Parameters.AddWithValue("@City", item.city);
-                        //    myCmd.ExecuteNonQuery();
-                        //}
-                        counter++;
-                        if(counter == array.length())
+                        cmd.Connection = mConnection;
+                        cmd.Transaction = tran;
+                        cmd.CommandText = "SELECT * FROM denemeTable";
+                        using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
                         {
-                            string tempCsv = @"dump.csv";
-                            using (StreamWriter w = new StreamWriter(tempCsv))
+                            da.UpdateBatchSize = 1000;
+                            using (MySqlCommandBuilder cb = new MySqlCommandBuilder(da))
                             {
-                                Rfc4180Writer.WriteDataTable(temporaryTable, w, false);
+                                //gecici tablo sql serverdaki tabloya aktarılıyor
+                                da.Update(temporaryTable);
+                                tran.Commit();
                             }
-                            var mBulk = new MySqlBulkLoader(mConnection);
-                            mBulk.TableName = "persons";
-                            mBulk.FileName = tempCsv;
-                            mBulk.FieldTerminator = ",";
-                            mBulk.FieldQuotationCharacter = '"';
-                            mBulk.Load();
-                            System.IO.File.Delete(tempCsv);
                         }
                     }
-                }
-                catch (Exception err)
-                {
-                    Console.WriteLine(err.Message);
                 }
             }
         }
     }
 }
 
-public static class Rfc4180Writer
-{
-    public static void WriteDataTable(DataTable sourceTable, TextWriter writer, bool includeHeaders)
-    {
-        if (includeHeaders)
-        {
-            IEnumerable<String> headerValues = sourceTable.Columns
-                .OfType<DataColumn>()
-                .Select(column => QuoteValue(column.ColumnName));
-
-            writer.WriteLine(String.Join(",", headerValues));
-        }
-
-        IEnumerable<String> items = null;
-
-        foreach (DataRow row in sourceTable.Rows)
-        {
-            items = row.ItemArray.Select(o => QuoteValue(o?.ToString() ?? String.Empty));
-            writer.WriteLine(String.Join(",", items));
-        }
-
-        writer.Flush();
-    }
-
-    private static string QuoteValue(string value)
-    {
-        return String.Concat("\"",
-        value.Replace("\"", "\"\""), "\"");
-    }
-}
